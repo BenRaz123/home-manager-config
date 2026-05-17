bf33751d1b91e9b60c3c047a7f8a781827b77998
BenRaz123 <ben.raz2008@gmail.com>
2026-05-17 03:45:56 +0000

    created flake
    Signed-off-by: BenRaz123 <ben.raz2008@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs =
+    inputs@{ nixpkgs, home-manager, ... }:
+    {
+      homeConfigurations.ben = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs;
+        extraSpecialArgs = { inherit inputs; };
+        modules = [ ./home.nix ];
+      };
+    };
+}
