b65d939baa6d2028980096b039d8fc12739a7264
BenRaz123 <ben.raz2008@gmail.com>
2026-06-17 16:27:38 +0000

    tmux: change terminal-features for linux if kmscon is being used
    Signed-off-by: BenRaz123 <ben.raz2008@gmail.com>

--- programs/tmux.nix
+++ programs/tmux.nix
@@ -1,4 +1,37 @@
-{ config, ... }:
+{
+  config,
+  lib,
+  nixosConfig ? null,
+  ...
+}:
+let
+  opt = c: x: if c then x else null;
+  mkFeatures =
+    features:
+    let
+      lines = lib.mapAttrsToList (
+        tpat: tfeatures:
+        lib.optionalString (tfeatures != null && tpat != null) (
+          let
+            featuresList = lib.concatStringsSep ":" tfeatures;
+          in
+          ''
+            set -as terminal-features ",${tpat}:${featuresList}"
+          ''
+        )
+      ) features;
+    in
+    lib.concatStringsSep "\n" lines;
+  fullFeatures = [
+    "256"
+    "RGB"
+  ];
+  termFeatures = mkFeatures {
+    linux = opt (nixosConfig != null && nixosConfig.services.kmscon.enable) fullFeatures;
+    "xterm*" = fullFeatures;
+    "screen*" = fullFeatures;
+  };
+in
 {
   programs.tmux = {
     shell = config.settings.shell;
@@ -16,12 +49,12 @@
       set -g window-status-current-format "#[bold]#[fg=blue][#I]"
       set -g status-right "#(date -I) (#(fish -c get_batt)%) #[fg=green,bold]<#S>#[nobold,fg=white]"
       set -g status-position top
-      set -as terminal-features ",*:256,RGB"
 
       bind h select-pane -L
       bind j select-pane -D
       bind k select-pane -U
       bind l select-pane -R
-    '';
+    ''
+    + termFeatures;
   };
 }
